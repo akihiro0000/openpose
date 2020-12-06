af7cf841c14b22bf6d60c14aca02c2459270060c
akihiro0000 <64446427+akihiro0000@users.noreply.github.com>
2020-12-06 23:25:13 +0900

    Update app.py

--- app.py
+++ app.py
@@ -136,6 +136,6 @@ if __name__ == '__main__':
     t = threading.Thread(target=detection_loop)
     t.start()
     
-    app.run(host='0.0.0.0',threaded=True,port=8080,debug=False)
+    app.run(host='0.0.0.0',threaded=True,port=1110,debug=False)
     print("--------------finished-----------------")
     mqtt_client.disconnect()
